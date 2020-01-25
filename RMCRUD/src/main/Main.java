ebb43211a9681f59b19251aff34c81dbd78e2c61
Salvador de Jesus Ron Osio <56851020+salvadorron@users.noreply.github.com>
2020-01-24 21:54:32 -0400

    Add files via upload
    Registro de Peliculas al estilo CRUD.
    Esta Aplicacion permite listar un conjunto de peliculas usando el sistema de arquitectura Modelo, Vista, Controlador.

---
+++ RMCRUD/src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import controllers.MovieController;
+
+/**
+ * 
+ * @author Salvador de J. Ron Osio
+ *
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MovieController controller = new MovieController();
+
+		controller.view();
+
+	}
+
+}
